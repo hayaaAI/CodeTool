72f88be110deafbc9cfbb6f63cec121266011357
hayaa <dev@hayaa-ai.com>
2018-06-01 17:27:22 +0800

    修正代码模板错误

--- Hayaa.AutoCode/Hayaa.CodeToll.FrameworkService.MultiStorey/SolutionFrameworkServer.cs
+++ Hayaa.AutoCode/Hayaa.CodeToll.FrameworkService.MultiStorey/SolutionFrameworkServer.cs
@@ -45,7 +45,7 @@ namespace Hayaa.CodeTool.FrameworkService.MultiStorey
             MakeCodeForService(tables, serviceTemplate, databaseConnection, databaseName, savePath);
             //生成controller层代码
             CodeTemplate viewServiceTemplate = codeTemplatee.SolutionTemplates.Find(ct => ct.GenCodeType == CodeType.ViewService);
-            MakeCodeForViewService(tables, serviceTemplate, databaseConnection, databaseName, savePath);
+            MakeCodeForViewService(tables, viewServiceTemplate, databaseConnection, databaseName, savePath);
             return result;
         }
         public FunctionResult<Solution> MakeCodeForModel(List<string> tables, CodeTemplate codeTemplate, string databaseConnection, string databaseName, string savePath)
@@ -246,7 +246,7 @@ namespace Hayaa.CodeTool.FrameworkService.MultiStorey
             code.Append(String.Format("@Autowired private I{0}Service {1}Service;", t.Name, ParseName(t.Name)));
             //分页
             code.Append("@RequestMapping(value = \"pager\")");
-            code.Append(String.Format("public TransactionResult<GridPager<{0}>> GetPager(int page, int size)  throws Exception  {{  AssertHelper.AssertRangInt(page,1,Integer.MAX_VALUE); AssertHelper.AssertRangInt(id,size,Integer.MAX_VALUE);TransactionResult<GridPager<{0}>> result = new TransactionResult<GridPager<{0}>>();GridPagerPamater<{0}SearchPamater> pamater = new GridPagerPamater<>();{0}SearchPamater sp = new {0}SearchPamater();pamater.setSearchPamater(sp);pamater.setCurrent(page);pamater.setPageSize(size);GridPager<{0}> serviceReusult = {1}Service.GetPager(pamater);if (serviceReusult.isActionResult() && serviceReusult.isHavingData()){{result.setData(serviceReusult);}}else{{result.setCode(103);result.setMessage(\"暂无数据\");}}return result;}}", t.Name, ParseName(t.Name)));
+            code.Append(String.Format("public TransactionResult<GridPager<{0}>> GetPager(int page, int size)  throws Exception  {{  AssertHelper.AssertRangInt(page,1,Integer.MAX_VALUE); AssertHelper.AssertRangInt(size,1,Integer.MAX_VALUE);TransactionResult<GridPager<{0}>> result = new TransactionResult<GridPager<{0}>>();GridPagerPamater<{0}SearchPamater> pamater = new GridPagerPamater<>();{0}SearchPamater sp = new {0}SearchPamater();pamater.setSearchPamater(sp);pamater.setCurrent(page);pamater.setPageSize(size);GridPager<{0}> serviceReusult = {1}Service.GetPager(pamater);if (serviceReusult.isActionResult() && serviceReusult.isHavingData()){{result.setData(serviceReusult);}}else{{result.setCode(103);result.setMessage(\"暂无数据\");}}return result;}}", t.Name, ParseName(t.Name)));
             //get
             code.Append(" @RequestMapping(value = \"get\")");
             code.Append(String.Format("public TransactionResult<{0}> Get(int id)  throws Exception  {{ AssertHelper.AssertRangInt(id,1,Integer.MAX_VALUE); TransactionResult<{0}> result = new TransactionResult<{0}>();FunctionResult<{0}> serviceResult = {1}Service.Get(id);if (serviceResult.isActionResult() && serviceResult.isHavingData()){{result.setData(serviceResult.getData());}}else{{result.setCode(103);result.setMessage(\"暂无数据\");}}return result;}}", t.Name, ParseName(t.Name)));
@@ -258,10 +258,10 @@ namespace Hayaa.CodeTool.FrameworkService.MultiStorey
             code.Append(String.Format("public TransactionResult<{0}> Add({0} info)  throws Exception {3}{2} TransactionResult<{0}> result = new TransactionResult<{0}>();FunctionResult<{0}> serviceResult = {1}Service.Create(info);if (serviceResult.isActionResult() && serviceResult.isHavingData()){3}result.setData(serviceResult.getData());{4}else{3}result.setCode(103);result.setMessage(\"暂无数据\");{4}return result;{4}", t.Name, ParseName(t.Name), CreateAssertCodeForJava(t), "{", "}"));
             //edit
             code.Append("@RequestMapping(value = \"edit\")");
-            code.Append(String.Format("public TransactionResult<Boolean> Edit({0} info)  throws Exception  {3}{2} TransactionResult <Boolean> result = new TransactionResult<Boolean>();FunctionResult <Boolean> serviceResult = {1}Service.UpdateByID(info);if (serviceResult.isActionResult()){3}result.setData(serviceResult.getData());{4}else{3}result.setCode(103);result.setMessage(\"暂无数据\");{4}return result;{4}", t.Name, ParseName(t.Name), CreateAssertCodeForJava(t), "{", "}"));
+            code.Append(String.Format("public TransactionResult<Boolean> Edit({0} info)  throws Exception  {3}{2} TransactionResult <Boolean> result = new TransactionResult<Boolean>();FunctionOpenResult <Boolean> serviceResult = {1}Service.UpdateByID(info);if (serviceResult.isActionResult()){3}result.setData(serviceResult.getData());{4}else{3}result.setCode(103);result.setMessage(\"暂无数据\");{4}return result;{4}", t.Name, ParseName(t.Name), CreateAssertCodeForJava(t), "{", "}"));
             //del
             code.Append("@RequestMapping(value = \"del\")");
-            code.Append(String.Format("public TransactionResult<Boolean> Delete(int id)  throws Exception  {2}AssertHelper.AssertRangInt(id,1,Integer.MAX_VALUE);TransactionResult <Boolean> result = new TransactionResult<Boolean>(); List<Integer> ids=new ArrayList<>();ids.add(id); FunctionResult <Boolean> serviceResult = {1}Service.DeleteByID(ids);if (serviceResult.isActionResult()){2}result.setData(serviceResult.getData());{3}else{2}result.setCode(103);result.setMessage(\"暂无数据\");{3}return result;{3}", t.Name, ParseName(t.Name), "{", "}"));
+            code.Append(String.Format("public TransactionResult<Boolean> Delete(int id)  throws Exception  {2}AssertHelper.AssertRangInt(id,1,Integer.MAX_VALUE);TransactionResult <Boolean> result = new TransactionResult<Boolean>(); List<Integer> ids=new ArrayList<>();ids.add(id); FunctionOpenResult<Boolean> serviceResult = {1}Service.DeleteByID(ids);if (serviceResult.isActionResult()){2}result.setData(serviceResult.getData());{3}else{2}result.setCode(103);result.setMessage(\"暂无数据\");{3}return result;{3}", t.Name, ParseName(t.Name), "{", "}"));
             code.Append("}");
             return code;
         }
